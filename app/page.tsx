c81bcfdc3977a2674896e504189d149a9c112590
ozan isgor <ozanisgor11@gmail.com>
2023-08-16 22:16:02 +0300

    UPDATE: page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import PokemonGrid from "./components/PokemonGrid";
+import Results from "@/app/components/Results";
 
 type HomeProps = {
   searchParams: {
@@ -7,12 +7,41 @@ type HomeProps = {
   };
 };
 
+async function getPokeData(limit: number, offset: number) {
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  );
+  const data = await res.json();
+  const results = data.results;
+
+  const pokemons = await Promise.all(
+    results.map(async (pokemon: any) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error("Something went wrong");
+      }
+      return data;
+    })
+  );
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return pokemons;
+}
+
 export const dynamic = "force-dynamic";
 
-export default function Home({ searchParams }: HomeProps) {
+export default async function Home({ searchParams }: HomeProps) {
+  const limit = searchParams.limit || 151;
+  const offset = searchParams.offset || 0;
+
+  const pokemons = await getPokeData(limit, offset);
   return (
     <div>
-      <PokemonGrid searchParams={searchParams} />
+      <Results pokemons={pokemons} />
     </div>
   );
 }
